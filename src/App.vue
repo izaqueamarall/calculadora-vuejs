a98bbab0967aaa0df81fbf9626834cd91a31e01e
Izaque Amaral de Araujo Silva <izaqueamaral01@gmail.com>
2022-11-11 15:55:32 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="main-card">
+
+  </div>
+</template>
+
+<script>
+import Calculator from "./components/Calculator.vue";
+export default {
+  data() {
+    return {
+
+    }
+  },
+  components: {
+    Calculator
+  }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Calculator.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="card-calculator">
+    <div class="display">
+        
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
