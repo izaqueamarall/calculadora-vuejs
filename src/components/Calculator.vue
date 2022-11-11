ab8a8d34a519b83b9cf278b97c26aa2f2c052cb1
Izaque Amaral de Araujo Silva <izaqueamaral01@gmail.com>
2022-11-11 16:42:40 -0300

    Update Calculator.vue

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="card-calculator">
     <div class="display">
-      <p>TESTE</p>
+      <p>DISPLAY</p>
     </div>
     <div class="calculator-panel">
       <div><button>7</button></div>
       <div><button>8</button></div>
       <div><button>9</button></div>
+      <div><button>+</button></div>
       <div><button>4</button></div>
       <div><button>5</button></div>
       <div><button>6</button></div>
+      <div><button>-</button></div>
       <div><button>1</button></div>
       <div><button>2</button></div>
       <div><button>3</button></div>
+      <div><button>x</button></div>
+      <div><button>0</button></div>
+      <div><button>,</button></div>
+      <div><button>=</button></div>
+      <div><button>/</button></div>
     </div>
   </div>
 </template>
@@ -31,6 +38,7 @@ background-color: black;
 }
 .display > p {
     color: white;
+    text-align: center;
 }
 .calculator-panel {
   display: flex;
